77889038653deafbf8db88604291561c5281cb6a
Paolo Possanzini <ppossanzini@gmail.com>
2016-02-28 22:33:36 +0100

    uu

---
+++ DeNSo.Client.InProcess/Commands/Raw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeNSo.Commands
+{
+  public class Raw : BaseCommand
+  {
+    private ObjectStore _store;
+    public Raw(Collection collection) { _store = StoreManager.GetObjectStore(collection.Database, collection.Name); }
+
+    public void Append(string key, byte[] value) { _store.Append(key, value); }
+    public byte[] Get(string key) { return _store.GetById(key); }
+    public void Set(string key, byte[] value) { _store.Set(key, value); }
+    public int Len(string key) { return _store.Len(key); }
+    public byte[] GetSet(string key, byte[] value) { return _store.GetSet(key, value); }
+    public IEnumerable<byte[]> Get(params string[] keys)
+    {
+      foreach (var r in _store.MultipleGet(keys))
+        yield return r;
+      yield break;
+    }
+  }
+}
