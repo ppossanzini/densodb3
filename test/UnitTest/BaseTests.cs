0a000920e693296730f8f6374b944392757147a5
paolo Possanzini <paolo@teamdev.it>
2016-02-28 22:41:56 +0100

    io bo

---
+++ test/UnitTest/BaseTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeNSo;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UnitTest
+{
+  [TestClass]
+  public class BaseTests
+  {
+    private const string dbName = "TestDB.denso";
+
+
+    [TestCleanup]
+    public void Clean()
+    {
+      ManualResetEvent wait = new ManualResetEvent(false);
+
+      Session.ShutDown();
+
+      ThreadPool.QueueUserWorkItem(w => { Thread.Sleep(5000); wait.Set(); });
+
+      wait.WaitOne();
+    }
+
+    [TestInitialize]
+    public void Init()
+    {
+      Session.DefaultDataBase = dbName;
+      Session.Start();
+    }
+
+    [TestMethod]
+    public void SimpleWrite()
+    {
+      Session.New.Collections["test"].Strings.Append("t1", "test");
+    }
+
+    [TestMethod]
+    public void WriteAndRead()
+    {
+      var s = Session.New;
+      s.Collections["test"].Strings.Append("t1", "test2");
+
+      Assert.AreEqual("test2", s.Collections["test"].Strings.Get("t1"));
+    }
+
+    [TestMethod]
+    public void WriteRawBigData()
+    {
+      var collection = Session.New.Collections["test"];
+
+      Stopwatch s = new Stopwatch();
+      s.Start();
+      var data = File.ReadAllBytes(@"c:\downloads\WhatsApp.apk");
+      s.Stop();
+
+      Console.WriteLine("Reading file: " + s.Elapsed.TotalMilliseconds);
+
+      s.Reset();
+      s.Start();
+      collection.RawData.Set("t1", data);
+      s.Stop();
+
+      Console.WriteLine("Writing to DB : " + s.Elapsed.TotalMilliseconds);
+
+      s.Reset();
+      s.Start();
+      data = collection.RawData.Get("t1");
+      s.Stop();
+
+      Console.WriteLine("Read from DB : " + s.Elapsed.TotalMilliseconds);
+
+    }
+
+    [TestMethod]
+    public void OnlyRead()
+    {
+      var collection = Session.New.Collections["test"];
+      Stopwatch s = new Stopwatch();
+      s.Start();
+      var data = collection.RawData.Get("t1");
+      s.Stop();
+
+      Console.WriteLine("Read from DB : " + s.Elapsed.TotalMilliseconds);
+      Console.WriteLine(data.Length.ToString());
+
+    }
+  }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,49 @@
+﻿using DeNSo;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+  class Program
+  {
+    private const string dbName = "TestDB.denso";
+    static void Main(string[] args)
+    {
+      
+      Session.DefaultDataBase = dbName;
+      Session.Start();
+
+      var collection = Session.New.Collections["test"];
+
+      Stopwatch s = new Stopwatch();
+      s.Start();
+      var data = File.ReadAllBytes(@"c:\downloads\script.sql");
+      s.Stop();
+
+      Console.WriteLine("Reading file: " + s.Elapsed.TotalMilliseconds);
+
+      s.Reset();
+      s.Start();
+      collection.RawData.Set("t1", data);
+      s.Stop();
+
+      Console.WriteLine("Writing to DB : " + s.Elapsed.TotalMilliseconds);
+
+      s.Reset();
+      s.Start();
+      data = collection.RawData.Get("t1");
+      s.Stop();
+
+      Console.WriteLine("Read from DB : " + s.Elapsed.TotalMilliseconds);
+
+      Session.ShutDown();
+      Session.Start();
+
+    }
+  }
+}
